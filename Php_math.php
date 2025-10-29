26a9f4f30eadc49ab2875f00f9163a429080e3ce
Fathma Khatun Mim <fathmakhatunmim@gmail.com>
2025-10-29 13:05:58 +0600

    math

---
+++ Php_math.php
@@ -0,0 +1,37 @@
+<?php
+
+echo "welcome to php math <br><br>";
+
+echo(pi());
+echo "<br><br>";
+
+
+
+echo "min and max function<br>";
+
+echo("min: ".min(0, 150, 30, 20, -8, -200));
+echo "<br>";
+echo("max: " .max(0, 150, 30, 20, -8, -200));
+echo "<br><br>";
+
+
+echo "absolute (positive) <br>";
+echo(abs(-6.7));
+echo "<br>";
+
+echo "square root of a number<br>";
+echo(sqrt(64));
+echo "<br>";
+
+echo "rounds a floating-point number<br>";
+echo(round(0.60));
+echo "<br>";
+
+
+echo "Random number<br>";
+echo(rand(10, 100));
+echo "<br>";
+
+
+
+?>
