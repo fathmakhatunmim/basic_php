a09f0408b4111836ba1ed1bcc858f7337269fa3e
Fathma Khatun Mim <fathmakhatunmim@gmail.com>
2025-10-29 16:53:52 +0600

    json

--- php_json.php
+++ php_json.php
@@ -1,9 +1,15 @@
 <?php
 
 $user = ["name" => "anil", "age" => 30, "email" => "anil@abc.com"];
-$userJson = json_encode($user);
+$userJson = json_encode($user);//array theke jeson a convort kore dau
 echo $userJson ;
+echo "<br>";
 
+
+$data = '{ "name": "anil" , "age" : 30 , "email":"anil@abc.com"}';
+// echo $data;
+$dataArray = json_decode($data);
+print_r($dataArray)
 
 
 
