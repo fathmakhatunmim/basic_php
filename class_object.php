1ae72fba8dfd5067d5f3635a08c63887be836051
Fathma Khatun Mim <fathmakhatunmim@gmail.com>
2025-10-30 10:08:54 +0600

    class_object

---
+++ class_object.php
@@ -0,0 +1,65 @@
+<?php
+
+//  class class1{
+
+//     public $a=2;
+
+//     function fun1(){
+//         // echo "fun1";
+//         // echo $this -> a;
+//         return $this -> a;
+//     }
+
+//  function fun2(){
+//         echo "fun1";
+//     }
+
+
+//  }
+
+//     $obj = new class1();
+//     // $obj -> fun1();
+//     echo $obj -> a;
+
+
+class class1 {
+    public $a = 1;
+
+    function fun1(){
+        return $this -> a++;
+
+    }
+
+}
+
+$obj1 = new class1();
+$obj2 = new class1();
+
+
+$obj1 -> fun1();
+echo $obj1-> a;
+
+echo "</br>";
+
+echo $obj2 -> a;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ ?>
