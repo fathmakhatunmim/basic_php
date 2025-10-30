e4c7c0b5998478a68e031bc26f06d274ab0ce334
Fathma Khatun Mim <fathmakhatunmim@gmail.com>
2025-10-30 13:23:47 +0600

    abstract

--- abstract_class.php
+++ abstract_class.php
@@ -1,12 +1,19 @@
 <?php
 
+abstract class parentClass{
 
+    public $name;
+    abstract protected function calc($a , $b);
+}
 
+class childClass extends  parentClass{
 
+   public function calc($c , $d)//abstruct jemon function declear korbo same to same funtion driveclass a bebohar korte hobe
+   {
+    echo $c + $d;
+   }
+}
 
-
-
-
-
-
+$test = new childClass();
+$test -> calc (20,30);
 ?>
