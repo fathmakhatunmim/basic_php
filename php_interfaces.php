cc1a28053638b60e62e4250047bab155d27a0590
Fathma Khatun Mim <fathmakhatunmim@gmail.com>
2025-10-30 15:06:01 +0600

    interfaces

---
+++ php_interfaces.php
@@ -0,0 +1,25 @@
+<?php 
+
+interface parent1{
+    //public dite parbo but private ar protected dite parbo na
+ public function calc($a ,$b);
+}
+
+
+interface parent2{
+    function sub($c ,$d);
+}
+
+class childClass implements parent1 , parent2 {
+    public function calc($a , $b){ 
+        echo $a + $b;
+    }
+
+ public function sub($c , $d){ 
+        echo $c - $d;
+    }
+}
+
+    $test = new childClass();
+    $test -> calc(20,35);
+?>
